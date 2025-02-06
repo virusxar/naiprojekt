022c6f3fafcebbd5ff961b30ea392aa24720e50d
Milewczik <s27271@pjwstk.edu.pl>
2025-02-06 14:09:39 +0100

    poprawka

--- uczenie_resnet50.py
+++ uczenie_resnet50.py
@@ -23,7 +23,7 @@ transform = transforms.Compose([
     transforms.RandomHorizontalFlip(),
     transforms.RandomRotation(15),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),a
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
 # Wczytanie danych treningowych i walidacyjnych
